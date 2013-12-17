b106745369eb5f3b18e920288eb2c1994d8d2def
rwh <ross.horsey@eduserv.org.uk>
2013-12-17 16:55:35 +0000

    more stuffs

--- DiceRoller/Content/js/hub.js
+++ DiceRoller/Content/js/hub.js
@@ -31,9 +31,10 @@
         rollerHub.init(diceroller);
         userHub.init(diceroller);
         // Get the user name and store it to prepend to messages.
-        user = prompt("Please enter your name");
+        //user = prompt("Please enter your name");
+        user = "anon";
         diceroller.state.userName = user;
-        $('#user').append(user);
+        $('#user').empty().append("not logged in");
         $('#displayname').val(user);
         // Set initial focus to message input box.  
         $('#message').focus();
@@ -55,6 +56,18 @@
 
         $('#setInit').click(function () {
             diceroller.server.setInit($('#diceroll').val());
+        });
+
+        $('#userlogin').blur(function () {
+            user = $('#userlogin').val();
+            diceroller.state.userName = user;
+            $('#user').empty().append("Logged in as " + user);
+            $('#displayname').val(user);
+            // Set initial focus to message input box.  
+            $('#message').focus();
+            diceroller.server.setName(user);
+            diceroller.server.getLog();
+            $('#userlogin').hide();
         });
 
         $('#sendmessage').click(function () {
