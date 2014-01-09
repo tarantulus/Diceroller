b9117c05de0d9bb022cedf06f5bfd86bf86a757d
rwh <ross.horsey@eduserv.org.uk>
2014-01-09 15:48:31 +0000

    used implicitly

--- DiceRoller/Hubs/CanvasHub.cs
+++ DiceRoller/Hubs/CanvasHub.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using DiceRoller.Annotations;
 using Microsoft.AspNet.SignalR;
 
 namespace DiceRoller.Hubs
 {
+    [UsedImplicitly]
     public class CanvasHub:Hub
     {
         public void SendCanvas(object img)
--- DiceRoller/Hubs/ChatHub.cs
+++ DiceRoller/Hubs/ChatHub.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using DiceRoller.Annotations;
 using Microsoft.AspNet.SignalR;
 
 namespace DiceRoller.Hubs
 {
+    [UsedImplicitly]
     public class ChatHub : Hub
     {
         public void Send(string name, string msg)
